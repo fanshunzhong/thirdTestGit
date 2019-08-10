e2f5ef8783d48b7215ee11dcb187b03d8d7f3a5b
fanshunzhong <824284472@qq.com>
2019-08-10 20:25:56 +0800

    用户1修改了代码

--- src/main/java/com/shunzhong/User.java
+++ src/main/java/com/shunzhong/User.java
@@ -3,6 +3,16 @@ package com.shunzhong;
 public class User {
     private String username;
     private String password;
+    /*用户1增加的字段age*/
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
     public String getUsername() {
         return username;
